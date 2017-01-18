91850cad13f2573e5015679e4fc8f50f525c737f
jakim54 <james.kim@huffingtonpost.com>
2017-01-18 17:36:30 -0500

    Took long to solve the logic out, but learned shit ton in process, also work took my time.

--- Extensions/Extensions.swift
+++ Extensions/Extensions.swift
@@ -10,15 +10,17 @@ import Foundation
 
 extension String {
     
-    func whisper(words: String) -> String {
-        return words.lowercased()
+    func whisper() -> String {
+        return self.lowercased()
     }
     
-    func shout(noises: String) -> String {
-        return noises.uppercased()
+    func shout() -> String {
+        return self.uppercased()
     }
     
+    // function for pigLatin
     // need to add regex filter
+    // use reduce/map/filter to refactor this
     func processWord(array processedArray: [String]) -> [String] {
         var returnArray = [String]()
         var toReturn = ""
@@ -36,18 +38,67 @@ extension String {
     }
     
     // refactor this with reduce/filter/map
+    // need to include punctuations/regex stuff filter
     var pigLatin: String {
         let toConvert = self
         
-        if toConvert.characters.count == 1 {
+        if toConvert.isEmpty || toConvert.characters.count == 1 {
             return toConvert
         } else {
             let arrayConverted = toConvert.components(separatedBy: " ")
-            var toReturn = ""
-            let convertToStrings = processWord(array: arrayConverted)
-            toReturn = convertToStrings.joined(separator: " ")
-            return toReturn
+            let convertedToStrings = processWord(array: arrayConverted).joined(separator: " ")
+            return convertedToStrings
         }
+    }
+    
+    // also need to be refactored to use reduce/filter/map.
+    var points: Int {
+        
+        // Start copy block
+        var sounds = [
+            "consonant" : ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z"],
+            "vowels" : ["a", "e", "i", "o", "u", "w", "y"]
+        ]
+        
+        var totalPoints = 0
+        if self.characters.count < 1 {
+            return totalPoints
+        } else {
+            
+            var valueToCheck = self.lowercased().components(separatedBy: .whitespaces).joined(separator: "")
+            let countOfValueToCheck = valueToCheck.characters.count
+            
+            // this is a Set converted from an Array of valueToCheck
+            let rippedCharToSet = Set(valueToCheck.characters.map { String($0) } )
+            
+            // this is an Array of valueToCheck
+            let rippedCharToArray = valueToCheck.characters.map { String($0) }
+            
+            if let
+                allVowels = sounds["vowels"],
+                let allConsonants = sounds["consonant"] {
+                var allVowels = allVowels
+                var allConsonants = allConsonants
+                
+                for eachLetter in rippedCharToArray {
+                    if allVowels.contains(String(eachLetter)) {
+                        totalPoints += 1
+                    }
+                    
+                    if allConsonants.contains(String(eachLetter)) {
+                        totalPoints += 2
+                    }
+                }
+                
+            } // end of if let statement
+        } // end of else block
+        return totalPoints
+        // end copy block
+        
+    } // end of points
+    
+    var length: Int {
+        return self.characters.count
     }
     
 } // end of extension
